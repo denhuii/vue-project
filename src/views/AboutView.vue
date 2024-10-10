d389ef5a60db17b1588b1fabf2dc12d75ded538a
H <873023670@qq.com>
2024-10-11 00:52:55 +0800

    init project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <AMap />
+</template>
+
+<script setup>
+import AMap from './Maps/AMap.vue'
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/Maps/AMap.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="container"></div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const map = ref({})
+
+// init AMap
+
+AMapLoader.load({
+  key: 'e2451f84296a948ed38d7e730f066c23',
+  version: '2.0'
+})
+  .then((AMap) => {
+    map.value = new AMap.Map('container', {
+      zoom: 10,
+      center: [116.397428, 39.90923]
+    })
+  })
+  .catch((e) => {
+    console.error(e)
+  })
+</script>
+
+<style scoped>
+#container {
+  width: 100%;
+  height: 100vh;
+}
+</style>
